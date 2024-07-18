73879652dbf12b18a8724537b226cf791a580e23
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-07-18 11:14:23 +0000

    fix: code refracted

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -11,6 +11,6 @@ const newMap = {
 
 export const map = Map(newMap);
 
-export const map2 = map
-  .set(2, 'Benjamin')
-  .set(4, 'Oliver');
+export const map2 = map.withMutations((values) => {
+  values.set(2, 'Benjamin').set(4, 'Oliver');
+});
