9846e73cfbbb61faf6703437e6631848c3db6a3c
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-07-18 11:00:43 +0000

    fix: semistandard style

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,16 +1,16 @@
-import { Map } from "./node_modules/immutable/dist/immutable";
+import { Map } from './node_modules/immutable/dist/immutable';
 
-const newMap =  {
-    1: 'Liam',
-    2: 'Noah',
-    3: 'Elijah',
-    4: 'Oliver',
-    5: 'Jacob',
-    6: 'Lucas',
-}
+const newMap = {
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas'
+};
 
-export const map = Map(newMap)
+export const map = Map(newMap);
 
 export const map2 = map
-    .set(2, 'Benjamin')
-    .set(4, 'Oliver');
+  .set(2, 'Benjamin')
+  .set(4, 'Oliver');
